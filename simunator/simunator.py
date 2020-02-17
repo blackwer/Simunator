0e5d4fdd6259ecab8d8b675afd8589372c1c1029
Robert Blackwell <rblackwell@flatironinstitute.org>
2020-02-17 12:01:01 -0500

    Add dummy class template

--- simunator/simunator.py
+++ simunator/simunator.py
@@ -1,3 +1,17 @@
+import itertools
 
-def test():
-    print("Moo")
+
+class Simunator:
+    def __init__(self):
+        self._params = {}
+
+    def add_param(self, key: str, shortname: str, description="", bounds=(0.0, 1.0)):
+        self._params[key] = {
+            "shortname": shortname,
+            "description": description,
+            "bounds": bounds,
+        }
+
+    def print_combos(self):
+        for keys in itertools.product(list(self._params.keys())):
+            print(keys)
